758f22119f2895e1a2085344d43c3da6305aa1cb
Tolunay Ortahisar <tolunayortahisar99@gmail.com>
2024-07-07 01:01:43 +0300

    .

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+import lxml
+from bs4 import BeautifulSoup
+import smtplib
+
+
+BUY_PRICE = 200
+
+url = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7"
+}
+
+response = requests.get(url, headers=header)
+
+soup = BeautifulSoup(response.content, "lxml")
+print(soup.prettify())
+
+price = soup.find(class_="a-offscreen").get_text()
+price_without_currency = price.split("$")[1]
+price_as_float = float(price_without_currency)
+print(price_as_float)
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP(YOUR_SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )
